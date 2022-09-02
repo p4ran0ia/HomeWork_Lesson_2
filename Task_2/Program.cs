ce41fe74ec8bcbb53a7c3d04ff27c106a26e5a01
Alexandr <crazy.top@mail.ru>
2022-09-02 17:52:02 +0300

    Task Complete

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿int GenerateRandomNumbers()
+{
+    int n = new Random().Next(1, 1000000);
+    return n;
+}
+
+void ThirdNumber()
+{
+    int n = GenerateRandomNumbers();
+    Console.WriteLine(n);
+    if(n < 100)
+    Console.WriteLine("no third number");
+    else
+    {
+        while(n >= 1000)
+        {
+            int a = n / 10;
+            n = a;
+        }
+        n = n % 10;  
+    }
+    Console.WriteLine(n); 
+}
+
+ThirdNumber();
+
+
