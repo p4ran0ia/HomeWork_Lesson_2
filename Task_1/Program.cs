b5b2e8f753ff90c0c18d4a091edfe51e51095e0a
Alexandr <crazy.top@mail.ru>
2022-09-02 15:20:37 +0300

    Task Complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿void Num()
+{
+    Console.WriteLine("Input number: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if(n > 99 && n < 1000)
+    Console.WriteLine(n / 10 % 10);
+    else
+    Console.WriteLine("error");
+
+}
+Num();
