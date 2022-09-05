42491987ba1318d59ac00873735d8108701c0e0e
Alexandr <crazy.top@mail.ru>
2022-09-05 10:34:22 +0300

    Task complete

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input number of the day of the week");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 0 && n < 6)
+Console.WriteLine("no");
+else if(n > 5 && n < 8)
+Console.WriteLine("yes");
+else
+Console.WriteLine("error");
